3ec4d06c584c7941d2191ae70003e5415c345e3f
jmyager <jmyager6@gmail.com>
2019-02-11 21:31:40 -0500

    added 1% cost of property per year

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ $(function() { //shorthand document.ready function
         console.log(data); //use the console for debugging, F12 in Chrome, not alerts
         let monthlycashFlow = (data[1].value - data[2].value).toFixed(2);
         let yearlyCashFlow = (monthlycashFlow * 12).toFixed(2);
-        let roi = ((yearlyCashFlow / data[3].value) * 100).toFixed(2);
+        let roi = (((yearlyCashFlow * 0.99) / data[3].value) * 100).toFixed(2);
         $("#monthlyCashFlow").text(monthlycashFlow);
         $("#yearlyCashFlow").text(yearlyCashFlow);
         $("#roi").text(roi);
